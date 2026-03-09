237aad63b00da116163005d04d0c8d6ce282d803
Avunu LLC <mail@avu.nu>
2026-03-09 16:41:13 -0400

    fix: don't send canceled messages

--- messaging/messaging/doctype/group_text_message/__init__.py
+++ messaging/messaging/doctype/group_text_message/__init__.py
@@ -1,23 +1,29 @@
 # Copyright (c) 2023, Avunu LLC and contributors
 # For license information, please see license.txt
 
+from typing import cast
+
 import frappe
 from frappe.query_builder import DocType
 from frappe.query_builder.functions import Now
 
+from .group_text_message import GroupTextMessage
+
 # Define DocTypes for query builder
-GroupTextMessage = DocType("Group Text Message")
+GroupTextMessages = DocType("Group Text Message")
 
 
 def send_scheduled_messages():
 	# get all of the group text messages that are scheduled to be sent where the delivery_datetime datetime is past or equal to now
 	group_text_messages = (
-		frappe.qb.from_(GroupTextMessage)
-		.select(GroupTextMessage.name)
-		.where(GroupTextMessage.delivery_datetime <= Now())
-		.where(GroupTextMessage.status == "Scheduled")
-	).run(as_list=True)[0]
+		frappe.qb.from_(GroupTextMessages)
+		.select(GroupTextMessages.name)
+		.where(GroupTextMessages.delivery_datetime <= Now())
+		.where(GroupTextMessages.docstatus == 1)
+		.where(GroupTextMessages.schedule == 1)
+		.where(GroupTextMessages.status == "Scheduled")
+	).run(pluck="name")
 
 	for group_text_name in group_text_messages:
 		# send the text message
-		frappe.get_doc("Group Text Message", group_text_name).send_text_message()
+		cast(GroupTextMessage, frappe.get_doc("Group Text Message", group_text_name)).send_text_message()
